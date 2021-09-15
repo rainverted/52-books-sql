62ded315467cac00504c40e99e09b4b915df0f2e
rainverted <MY_NAME@example.com>
2021-09-15 19:16:52 +0300

    Create author

--- Author.js
+++ Author.js
@@ -1,9 +1,20 @@
 const Author = {};
 
 Author.create = async (connection, authorFirstname, authorLastname) => {
+    //sql uzklausa rasoma su const, nes vykdoma funkcijoje
+    const sql = 'INSERT INTO `authors`\
+                (`id`, `firstname`, `lastname`)\
+                VALUES (NULL, "'+ authorFirstname + '", "' + authorLastname + '")';
+    const [rows] = await connection.execute(sql);
+    const res = `Naujas autorius pridetas: ${authorFirstname} ${authorLastname} `;
+    console.log(res);
+    return res;
+    console.log('');
 }
 
 Author.listAll = async (connection) => {
+
+
 }
 
 Author.findById = async (connection, authorId) => {
